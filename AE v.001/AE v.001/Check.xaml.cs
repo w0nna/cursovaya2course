bf652b252aeea914e56058b8c30d84a534df794f
w0nna <bugreyev.glebkp13@gmail.com>
2023-05-06 00:16:58 +0300

    06.05 without patterns

---
+++ AE v.001/AE v.001/Check.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AE_v._001
+{
+    /// <summary>
+    /// Логика взаимодействия для Check.xaml
+    /// </summary>
+    public partial class Check : Window
+    {
+        public Check()
+        {
+            InitializeComponent();
+        }
+
+        private void buyoficcial1_Click(object sender, RoutedEventArgs e)
+        {
+            Window mw = new Window();
+            mw.Show();
+
+
+        }
+    }
+}
